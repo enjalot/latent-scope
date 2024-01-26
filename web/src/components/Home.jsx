25422eef906a32ef3ba241de571ac06bf791aa5f
Ian Johnson <enjalot@gmail.com>
2024-01-26 17:44:40 -0500

    slightly more styling on home scopes

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -119,13 +119,15 @@ function Home() {
         {datasets.map(dataset => (
           <li key={dataset.id}>
             <Link to={`/datasets/${dataset.name}/setup`}>Setup {dataset.name}</Link>
+            <div className="scope-links">
             {scopes[dataset.id] && scopes[dataset.id].map && scopes[dataset.id]?.map((scope,i) => (
-              <div key={i} >
+              <div className="scope-link" key={i} >
                 <Link to={`/datasets/${dataset.name}/explore/${scope.id}`}>Explore {scope.name} - {scope.label}<br></br>
                 <img width="120px" src={`${apiUrl}/files/${dataset.id}/clusters/${scope.cluster}.png`} />
                 </Link><br></br>
               </div>
             ))}
+            </div>
           </li>
         ))}
       </ul>
