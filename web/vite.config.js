88976f244ede85577342bd739fd2cef89797d7d5
Ian Johnson <enjalot@gmail.com>
2024-01-29 13:54:44 -0500

    fix vite config for read_only

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const mode = process.env.MODE || 'development';
 // import wasm from 'vite-plugin-wasm'
 // import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig(({ mode }) => ({
+  base: mode === 'read_only' ? '/latent-scope/' : '/',
   plugins: [react()],
   build: {
     outDir: `dist/${mode}`,
