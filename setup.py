b0453ba7faf919684a1873210ce7b41cc7cb01a3
Ian Johnson <enjalot@gmail.com>
2024-03-21 15:06:15 -0400

    bump to 0.1.8

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class CustomBuild(build_py):
 
 setup(
     name='latentscope',
-    version='0.1.7',
+    version='0.1.8',
     description='Quickly embed, project, cluster and explore a dataset.',
     long_description=long_description,
     long_description_content_type='text/markdown',
