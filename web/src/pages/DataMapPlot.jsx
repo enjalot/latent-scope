21170b579a24a28e1e4eef7a1e13a168db460fb9
dhruv-anand-aintech <dhruv.anand@ainorthstartech.com>
2024-06-26 15:46:24 +0530

    add options for fonts

--- web/src/pages/DataMapPlot.jsx
+++ web/src/pages/DataMapPlot.jsx
@@ -22,7 +22,7 @@ function niceBytes(bytes) {
 
 function DataMapPlot() {
   const [dataset, setDataset] = useState(null);
-  const { dataset: datasetId, scope: scopeId } = useParams(); 
+  const { dataset: datasetId, scope: scopeId } = useParams();
 
   const [plotJob, setPlotJob] = useState(null);
   const { startJob: startPlotJob } = useStartJobPolling(dataset, setPlotJob, `${apiUrl}/jobs/plot`);
@@ -115,7 +115,7 @@ function DataMapPlot() {
   }, [config, startPlotJob, scopeId]);
 
   useEffect(() => {
-    if(plotJob && plotJob.status === "completed") {
+    if (plotJob && plotJob.status === "completed") {
       console.log("plot job completed", plotJob)
       fetchPlotFiles();
     }
@@ -123,19 +123,19 @@ function DataMapPlot() {
 
 
 
-  const fileLink = useCallback((d,i) => {
+  const fileLink = useCallback((d, i) => {
     return <li className={styles["file-link"]} key={i}>
-      <a className={styles["config-link"]}href={`${apiUrl}/files/${datasetId}/plots/${d[0]}`} target="_blank" rel="noreferrer">
+      <a className={styles["config-link"]} href={`${apiUrl}/files/${datasetId}/plots/${d[0]}`} target="_blank" rel="noreferrer">
         {d[0]}
         <br></br>
         <img src={`${apiUrl}/files/${datasetId}/plots/${d[0]}`} alt={d[0]} width={400} />
       </a>
-      <br/>
+      <br />
       <span className={styles["size"]}>{niceBytes(d[2])} PNG</span>
       <span className={styles["config-link"]}><a href={`${apiUrl}/files/${datasetId}/plots/${d[0].split(".png")[0]}.json`} target="_blank" rel="noreferrer">
         [Config JSON]</a></span>
       {/* <span className={styles["path"]}>{d[1]}</span> */}
-      </li>
+    </li>
   }, [datasetId])
 
   return (
@@ -155,37 +155,37 @@ function DataMapPlot() {
           <div className={styles["label-group"]}>
             <label>
               <span>
-              Dark Mode:
-            </span>
-            <input type="checkbox" name="darkmode" checked={config.darkmode} onChange={handleChange} />
-          </label>
+                Dark Mode:
+              </span>
+              <input type="checkbox" name="darkmode" checked={config.darkmode} onChange={handleChange} />
+            </label>
             <label>
               <span>
-              DPI:
+                DPI:
               </span>
               <input type="number" name="dpi" value={config.dpi} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Figure Size Width:
+                Figure Size Width:
               </span>
               <input type="number" name="figsize.0" value={config.figsize[0]} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Figure Size Height:
+                Figure Size Height:
               </span>
               <input type="number" name="figsize.1" value={config.figsize[1]} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Label Wrap Width:
+                Label Wrap Width:
               </span>
               <input type="number" name="label_wrap_width" value={config.label_wrap_width} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Point Size:
+                Point Size:
               </span>
               <input type="number" name="point_size" value={config.point_size} onChange={handleChange} />
             </label>
@@ -194,77 +194,92 @@ function DataMapPlot() {
           <div className={styles["label-group"]}>
             <label>
               <span>
-              Label Over Points:
-            </span>
-            <input type="checkbox" name="label_over_points" checked={config.label_over_points} onChange={handleChange} />
-          </label>
-          <label>
-            <span>
-            Dynamic Label Size:
-            </span>
-            <input type="checkbox" name="dynamic_label_size" checked={config.dynamic_label_size} onChange={handleChange} />
-          </label>
+                Label Over Points:
+              </span>
+              <input type="checkbox" name="label_over_points" checked={config.label_over_points} onChange={handleChange} />
+            </label>
             <label>
               <span>
-              Max Font Size:
+                Dynamic Label Size:
+              </span>
+              <input type="checkbox" name="dynamic_label_size" checked={config.dynamic_label_size} onChange={handleChange} />
+            </label>
+            <label>
+              <span>
+                Max Font Size:
               </span>
               <input type="number" name="max_font_size" value={config.max_font_size} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Min Font Size:
+                Min Font Size:
               </span>
               <input type="number" name="min_font_size" value={config.min_font_size} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Min Font Weight:
+                Min Font Weight:
               </span>
               <input type="number" name="min_font_weight" value={config.min_font_weight} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Max Font Weight:
+                Max Font Weight:
               </span>
               <input type="number" name="max_font_weight" value={config.max_font_weight} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Font Family:
+                Font Family:
               </span>
-              <input type="text" name="font_family" value={config.font_family} onChange={handleChange} />
+              <select name="font_family" value={config.font_family} onChange={handleChange}>
+                <option value="Roboto Condensed">Roboto Condensed</option>
+                <option value="Arial">Arial</option>
+                <option value="Helvetica">Helvetica</option>
+                <option value="Times New Roman">Times New Roman</option>
+                <option value="Courier New">Courier New</option>
+                <option value="Verdana">Verdana</option>
+                <option value="Georgia">Georgia</option>
+                <option value="Palatino">Palatino</option>
+                <option value="Garamond">Garamond</option>
+                <option value="Bookman">Bookman</option>
+                <option value="Comic Sans MS">Comic Sans MS</option>
+                <option value="Trebuchet MS">Trebuchet MS</option>
+                <option value="Arial Black">Arial Black</option>
+                <option value="Impact">Impact</option>
+                <option value="Lucida Sans Unicode">Lucida Sans Unicode</option>
+              </select>
             </label>
           </div>
-
 
           <div className={styles["label-group"]}>
             <label>
               <span>
-              Add Glow:
+                Add Glow:
               </span>
               <input type="checkbox" name="add_glow" checked={config.add_glow} onChange={handleChange} />
             </label>
-          <label>
-            <span>
-            Kernel Bandwidth:
-            </span>
-            <input type="number" name="glow_keywords.kernel_bandwidth" step="0.01" value={config.glow_keywords.kernel_bandwidth} onChange={handleChange} />
-          </label>
             <label>
               <span>
-              Kernel:
+                Kernel Bandwidth:
+              </span>
+              <input type="number" name="glow_keywords.kernel_bandwidth" step="0.01" value={config.glow_keywords.kernel_bandwidth} onChange={handleChange} />
+            </label>
+            <label>
+              <span>
+                Kernel:
               </span>
               <input type="text" name="glow_keywords.kernel" value={config.glow_keywords.kernel} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Number of Levels:
+                Number of Levels:
               </span>
               <input type="number" name="glow_keywords.n_levels" value={config.glow_keywords.n_levels} onChange={handleChange} />
             </label>
             <label>
               <span>
-              Max Alpha:
+                Max Alpha:
               </span>
               <input type="number" name="glow_keywords.max_alpha" step="0.01" value={config.glow_keywords.max_alpha} onChange={handleChange} />
             </label>
@@ -273,11 +288,10 @@ function DataMapPlot() {
             <button type="submit">Create Plot</button>
           </div>
         </form>
-        
 
-        <JobProgress job={plotJob} clearJob={()=> {
+        <JobProgress job={plotJob} clearJob={() => {
           setPlotJob(null)
-        }}  />
+        }} />
 
         {plotJob && plotJob.status !== "completed" ? <p className={styles["warning"]}>
           May take more than 30 seconds to see progress.
