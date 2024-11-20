31d7d025a37b5bf67ab4c5ab71d5af22c2354cf4
Jimmy Zhang <jzhang621@gmail.com>
2024-11-20 15:06:41 -0500

    add buttons to left pane

--- web/src/components/Explore/LeftPane.jsx
+++ web/src/components/Explore/LeftPane.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './LeftPane.css';
+import { Icon, Button } from 'react-element-forge';
 
 export default function LeftPane() {
-  return <div className="left-pane-container">LeftPane</div>;
+  const filterIcon = <Icon name="filter" color="black" />;
+
+  const infoIcon = <Icon name="info" color="black" />;
+
+  const editIcon = <Icon name="edit" color="green" />;
+
+  return (
+    <div className="left-pane-container">
+      {infoIcon}
+      {filterIcon}
+      {editIcon}
+      {/* <Button icon={infoIcon} color="secondary" />
+      <Button icon={filterIcon} color="secondary" /> */}
+    </div>
+  );
 }
