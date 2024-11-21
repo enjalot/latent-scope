2338b505b7ab7e16347db38fd3a7f6c845bb756c
Jimmy Zhang <jzhang621@gmail.com>
2024-11-20 21:22:13 -0500

    left pane

--- web/src/components/Explore/LeftPane.jsx
+++ web/src/components/Explore/LeftPane.jsx
@@ -5,36 +5,39 @@ import { Button } from 'react-element-forge';
 export default function LeftPane() {
   return (
     <div className="left-pane-container">
-      <Button
-        className="left-pane-button"
-        size="small"
-        icon="info"
-        color="secondary"
-        title="View scope metadata"
-      />
-      <Button
-        className="left-pane-button"
-        size="small"
-        icon="table"
-        color="secondary"
-        title="Filter data points"
-      />
-      <Button
-        className="left-pane-button disabled"
-        size="small"
-        icon="pen-tool"
-        color="secondary"
-        title="Annotate"
-        disabled
-      />
-      <Button
-        className="left-pane-button disabled"
-        size="small"
-        icon="edit"
-        color="secondary"
-        title="Edit data points"
-        disabled
-      />
+      <div className="button-column main-buttons">
+        <Button
+          className="left-pane-button"
+          size="small"
+          icon="table"
+          color="secondary"
+          title="Filter data points"
+        />
+        <Button
+          className="left-pane-button disabled"
+          size="small"
+          icon="edit"
+          color="secondary"
+          title="View scope metadata"
+        />
+        <Button
+          className="left-pane-button disabled"
+          size="small"
+          icon="pen-tool"
+          color="secondary"
+          title="Annotate"
+          disabled
+        />
+      </div>
+      <div className="button-column info-button">
+        <Button
+          className="left-pane-button"
+          size="small"
+          icon="info"
+          color="secondary"
+          title="Edit data points"
+        />
+      </div>
     </div>
   );
 }
