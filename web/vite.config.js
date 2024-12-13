4b86e127098cbc52c63dfa58e2607424f1db2b88
Ian Johnson <enjalot@gmail.com>
2024-12-13 09:11:16 -0500

    fix build issue for wasm

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 const mode = process.env.MODE || 'development';
-// import wasm from 'vite-plugin-wasm'
-// import topLevelAwait from "vite-plugin-top-level-await";
+
 export default defineConfig(({ mode }) => ({
   base: mode === 'read_only' ? '/latent-scope/' : '/',
-  plugins: [react()],
+  plugins: [react(), wasm(), topLevelAwait()],
   build: {
     outDir: `dist/${mode}`,
   },
